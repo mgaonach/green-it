9df121bf0827d24ae13184dde465ab752e67b81e
Barthélémy Tek <barthtek@gmail.com>
2021-01-12 10:06:12 +0100

    q3

--- TP2.md
+++ TP2.md
@@ -4,3 +4,21 @@
 En état normal : environ 1OmA
 
 ## Q2
+Voir tableau excel
+
+## Q3
+9,7µA avec la config de la fonction enterSleepMod() :
+
+```c++
+void enterSleepMode() {
+
+  flash.powerDown();
+
+  LoRa.sleep();
+  
+  analogWrite(1, 0);
+
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+
+};
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,37 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <EEPROM.h>
-#include <bouton.cpp>
 #include <T2WhisperNode.h>
-#include <ledNumber.cpp>
 #include <LowPower.h>
+#include <LoRa.h>
+
+T2Flash flash;
+
+void enterSleepMode() {
+
+  flash.powerDown();
+
+  LoRa.sleep();
+  
+  analogWrite(1, 0);
+
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+
+};
 
 void setup() {
   
   Serial.begin(9600);
 
-  LowPower.powerDown(SLEEP_FOREVER, ADC_ON, BOD_ON);
+  LoRa.setPins(10,7,2);
+  if (!LoRa.begin(868E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
 
+  flash.init(T2_WPN_FLASH_SPI_CS);
+
+  enterSleepMode();
 };
 
 void loop() {

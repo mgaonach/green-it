d0d996c2bb2e86b6717336efd6a66feb3aef4645
Barthélémy Tek <barthtek@gmail.com>
2021-01-12 11:55:15 +0100

    TP2 End

--- TP2.md
+++ TP2.md
@@ -22,3 +22,26 @@ void enterSleepMode() {
 
 };
 ```
+
+## Q4
+Mesure de température : pic à 12mA
+Mesure endormi : ~45 µA
+
+## Q5
+
+(60000ms*40µA + 100ms*12000µA)/60100ms = 64,9µA
+240000µAh/64,9µA = 3698h
+
+Notre WhisperNode peut tenir 3698h sur la pile bouton, soit 154 jours.
+
+## Q6
+
+On mesure 5V.
+
+## Q8 
+
+Avec 2DB : 53.3 mA
+Avec 17DB : 125 mA
+Avec 20DB : 126.4 mA
+
+En terme de consommation, il vaut mieux régler la puissance d'émission le plus bas possible, bien que cela limite la portée du signal.
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,11 @@
 #include <T2WhisperNode.h>
 #include <LowPower.h>
 #include <LoRa.h>
+#include <RTClibExtended.h>
+#include <ledNumber.cpp>
 
 T2Flash flash;
+RTC_DS3231 rtc;
 
 void enterSleepMode() {
 
@@ -15,25 +18,76 @@ void enterSleepMode() {
   
   analogWrite(1, 0);
 
-  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
-
+  for (int i=0; i <8; i++)  {
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); 
+  }
 };
+
+void sendRadioMessage() {
+  const uint8_t radioBuf[10]={'0','1','2','3','4','5','6','7','8','9'};
+  LoRa.beginPacket();
+  LoRa.write(radioBuf,10);
+  LoRa.endPacket();
+}
+
+void checkTemp() {
+
+  float Ftemperature=rtc.getTemp();
+  Serial.println((int) Ftemperature);
+
+  LedNumber ledNumber = LedNumber(6, 9);
+  ledNumber.printOnLed((int) Ftemperature);
+};
+
+void measureVoltage() {
+  double voltageFTDI = (7.282 * analogRead(A7)) / 1024;
+  Serial.print("Voltage FTDI : ");
+  Serial.println(voltageFTDI);
+}
 
 void setup() {
   
   Serial.begin(9600);
 
+  // SETUP RTC
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+  rtc.adjust(DateTime(__DATE__, __TIME__));   //set RTC date and time to COMPILE time
+
+  // SETUP LoRA
   LoRa.setPins(10,7,2);
   if (!LoRa.begin(868E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
 
-  flash.init(T2_WPN_FLASH_SPI_CS);
+  // Q8
+  // LoRa.setTxPower(2);
+  // LoRa.setTxPower(20);
 
-  enterSleepMode();
+  // SETUP FLASH
+  flash.init(T2_WPN_FLASH_SPI_CS);
+  rtc = RTC_DS3231();
+  
+  // SETUP FOR VOLTAGE MEASURMENT
+  analogReference(INTERNAL);
+  pinMode(A7, INPUT);
+  digitalWrite(14, HIGH);
+
+  // Q7
+  for (int i=0; i <100; i++)  {
+    sendRadioMessage();
+  }
 };
 
 void loop() {
+  // checkTemp();
+  // enterSleepMode();
 
+  // Q6
+  // measureVoltage();
+
+  
 };

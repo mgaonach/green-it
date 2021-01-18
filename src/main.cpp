d7c56264503b2af59dccfeb02e3be91c9f031b9e
Barthélémy Tek <barthtek@gmail.com>
2021-01-18 14:43:00 +0100

    TP4 init

--- src/main.cpp
+++ src/main.cpp
@@ -1,93 +1,127 @@
+// Definition de la taille max du payload
+#define T2_MESSAGE_MAX_DATA_LEN 15
 #include <Arduino.h>
-#include <SPI.h>
-#include <EEPROM.h>
+#include <RH_RF95.h>
 #include <T2WhisperNode.h>
-#include <LowPower.h>
 #include <LoRa.h>
-#include <RTClibExtended.h>
-#include <ledNumber.cpp>
+#include <bouton.cpp>
 
-T2Flash flash;
-RTC_DS3231 rtc;
+////////////////////////////////////////////
+/// Configuration WhisperNode
+int myNetID = 1; // Definition du réseau
+int myID = 0; // ID du WhisperNode
+int mySerialNumber = 16; // Numero de série A CHANGER !!!
+////////////////////////////////////////////
+////////////////////////////////////////////
 
-void enterSleepMode() {
+// Radio
+uint8_t radioBuf[(T2_MESSAGE_HEADERS_LEN + T2_MESSAGE_MAX_DATA_LEN)];
+T2Message myMsg;
 
-  flash.powerDown();
+// Mes 2 boutons
+Bouton myButtonD(T2_WPN_BTN_2);
+Bouton myButtonG(T2_WPN_BTN_1);
 
-  LoRa.sleep();
-  
-  analogWrite(1, 0);
+// Tableau stockant les valeurs du payload
+char *array[10];
+int arrayint[10];
 
-  for (int i=0; i <8; i++)  {
-    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); 
-  }
-};
-
-void sendRadioMessage() {
-  const uint8_t radioBuf[10]={'0','1','2','3','4','5','6','7','8','9'};
-  LoRa.beginPacket();
-  LoRa.write(radioBuf,10);
-  LoRa.endPacket();
+// fonction permettant d'analyser le payload d'une trame
+// stocke les valeurs dans le tableau arrayint
+// premier parametre dans arrayint[0], deuxième dans arrayint[1] etc...
+int parseString (char *s) {
+    int i = 0;
+    char *p = strtok (s, ";");
+    while ((p != NULL) & (i<9)) {
+        array[i++] = p;
+        p = strtok (NULL, ";");
+    }
+    while (i<10) array[i++] = NULL;
+    for (i = 0; i < 10; ++i) {
+      if (array[i]!=NULL) {
+        arrayint[i]=atoi(array[i]);
+        }
+    }
+    return 0;
 }
 
-void checkTemp() {
+int sendLORA(int idx,int src, int dst, int sdx, int cmd, const char *data,int len) {
+	uint8_t radioBufLen = 0;
 
-  float Ftemperature=rtc.getTemp();
-  Serial.println((int) Ftemperature);
+  // Q4 A completer
+  // ...
 
-  LedNumber ledNumber = LedNumber(6, 9);
-  ledNumber.printOnLed((int) Ftemperature);
-};
+  LoRa.beginPacket();
+  LoRa.write(radioBuf,radioBufLen);
+  LoRa.endPacket();
+  return 1;
+}
 
-void measureVoltage() {
-  double voltageFTDI = (7.282 * analogRead(A7)) / 1024;
-  Serial.print("Voltage FTDI : ");
-  Serial.println(voltageFTDI);
+char buf[10]; // Buffeur utilisé pour l'envois de la trame
+int len;      // longueur de la trame
+
+int sendGiveMeANodeID(){
+  // Q5 A completer
+  // ...
+}
+
+int sendGiveMeAChannelAndField(){
+  // Q9 a completer
+}
+
+int sendMyValue(int value){
+  // Q11 a completer
+
 }
 
 void setup() {
-  
-  Serial.begin(9600);
+    Serial.begin(9600);
+    Serial.println("Radio Init");
+    // Q2 Initialisation Puce LoRa
+    // ...
+}
 
-  // SETUP RTC
-  if (!rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    while (1);
-  }
-  rtc.adjust(DateTime(__DATE__, __TIME__));   //set RTC date and time to COMPILE time
+char message[255]; //buffeur de reception d'un message
+uint8_t lmes; //longeur du message recu
 
-  // SETUP LoRA
-  LoRa.setPins(10,7,2);
-  if (!LoRa.begin(868E6)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  }
+int receivLoRa(){
+  // Q6 Reception d'une trame et construction de l'objet myMsg.
+  // ...
+}
 
-  // Q8
-  // LoRa.setTxPower(2);
-  // LoRa.setTxPower(20);
-
-  // SETUP FLASH
-  flash.init(T2_WPN_FLASH_SPI_CS);
-  rtc = RTC_DS3231();
-  
-  // SETUP FOR VOLTAGE MEASURMENT
-  analogReference(INTERNAL);
-  pinMode(A7, INPUT);
-  digitalWrite(14, HIGH);
-
-  // Q7
-  for (int i=0; i <100; i++)  {
-    sendRadioMessage();
-  }
-};
 
 void loop() {
-  // checkTemp();
-  // enterSleepMode();
+  /// RECEPTION
+  if (receivLoRa()==1) {
+      // Recoit on un ID ???
+      // Q7 Reception d'un ID
+      // tester les champs avec la bonne valeur.
+      if ((myMsg.idx == 0x00)&&(myMsg.dst==0x00)&&(myMsg.src==0x00)&&(myMsg.sdx==0x00)) {
+          parseString((char *) myMsg.data);
+          Serial.print("Reception d'un ID : ");
+          Serial.print(arrayint[0]);
+          Serial.print(" NETID : ");
+          Serial.print(arrayint[1]);
+          Serial.print(" au numero de serie : ");
+          Serial.println(arrayint[2]);
 
-  // Q6
-  // measureVoltage();
+      // Q7 puis si le numéro de série est le bon affecter myID
+      }
 
-  
-};
+      // Recoit on un Channel ???
+      // Q10 tester les champs avec la bonne valeur.
+      // puis afficher les valeurs
+      if ((myMsg.idx == 0x00)&&(myMsg.dst==0x00)&&(myMsg.src==0x00)&&(myMsg.sdx==0x00)) {
+      // Q10 a completer
+      }
+
+    }
+    /// FIN PARTIE RECEPTION
+
+  /// DEMANDE ID si Bouton droit appuyé
+  if (myButtonD.getNumber() >= 1) {
+      myID=0;myNetID=1;
+      Serial.println("J'ai besoin d'un ID");
+      sendGiveMeANodeID();
+  }
+}
